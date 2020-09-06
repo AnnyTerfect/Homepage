c57d20b571b247c24e16cfcde3e2591c3a024024
AnnyTerfect <1247470246@qq.com>
2020-09-06 09:04:16 +0800

    First commit

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+  "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+<meta name="generator" content="jemdoc, see http://jemdoc.jaboc.net/" />
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+<link rel="stylesheet" href="jemdoc.css" type="text/css" />
+
+<title>Qin-Cheng Zheng @ LAMDA, NJU-CS</title>
+</head>
+    <body>
+        <div id="layout-content">
+            <div id="toptitle">
+                <h1>Qin-Cheng Zheng @ LAMDA, NJU-CS</h1>
+            </div>
+            <table class="imgtable">
+                <tr>
+                    <td>
+                        <a href="./images/zqc.jpg"><img src="./images/zqc.jpg" alt="Qin-Cheng Zheng" width="180px" height="240px" /></a>&nbsp;
+                    </td>
+                    <td align="left">
+                        <p>
+                            <b><font size="+4" face="Kaiti">郑钦城</font></b>
+                            <br />
+                            <font size="+2">Qin-Cheng Zheng</font> <font size="+2">(Q.-C. Zheng)</font><br />
+                        </p>
+                        Ph.D. Student, <a href="http://lamda.nju.edu.cn/CH.MainPage.ashx">LAMDA Group</a>
+                        <br />
+                        <a href="http://CS.nju.edu.cn/">Department of Artificial Intelligence</a>
+                        <br />
+                        <a href="http://keysoftlab.nju.edu.cn/">National Key Laboratory for Novel Software Technology</a>
+                        <br />
+                        <a href="http://www.nju.edu.cn/">Nanjing University</a>, Nanjing 210023, China<br /><br />
+                        Supervisor: <a href="https://cs.nju.edu.cn/jiangy/" target="_blank"> Yuan Jiang</a> <br /><br />
+                        Email: zhumx@lamda.nju.edu.cn <br />
+                        Laboratory: Computer Science Building, Xianlin Campus of Nanjing University</p>
+                    </td>
+                    <td valign="top" width="179">
+                        <p align="center">
+                            <a href="http://lamda.nju.edu.cn/" target="_blank"><img height="85" src="./images/lamda.jpg" width="179" border="0" align="top"></a>
+                        </p>
+                        <p align="center">
+                            <a href="http://www.nju.edu.cn/" target="_blank"><img height="120" src="./images/nju.jpg" width="100" border="0"></a>
+                        </p>
+                    </td>
+                </tr>
+            </table>
+            <h2>Supervisor</h2>
+                <p>Professor <a href="http://cs.nju.edu.cn/wangwei/">Yuan Jiang</a></p>
+
+            <h2>Biography</h2>
+            <ul>
+                <li><p>Currently I am first-year graduate student of
+                    <a href="http://cs.nju.edu.cn">Department of Computer Science and Technology</a> in
+                    <a href="http://www.nju.edu.cn/">Nanjing University</a> and a member of
+                    <a href="http://lamda.nju.edu.cn/">LAMDA Group</a>, led by professor
+                    <a href="http://cs.nju.edu.cn/zhouzh/">Zhi-Hua Zhou</a>.
+                </p></li>
+                <li><p>I got my B.Sc. degree in <b>Statistics</b> from <a href="http://maths.hust.edu.cn/">School of Mathematics and Statistics, Huazhong University of Science and Technology (HUST)</a>, China in June 2020.
+                </p></li>
+            </ul>
+                
+            <h2>Research Interests</h2>
+                <p>My research interests include Machine Learning and Data Mining.  </p>
+                
+            <h2>Correspondence</h2>
+                <h3>Email:</h3>
+                <p>
+                    zhengqc@lamda.nju.edu.cn<br />
+                </p>
+                <h3>Laboratory:</h3>
+                    <p>Room 113, Computer Science Building, Xianlin Campus of Nanjing University</p>
+                <h3>Address:</h3>
+                <p>
+                    Qin-Cheng Zheng<br />
+                    National Key Laboratory for Novel Software Technology,<br />
+                    Nanjing University, Xianlin Campus, <br />
+                    163 Xianlin Avenue, Qixia District,<br />
+                    Nanjing 210023, China</p>
+                    (江苏省南京市栖霞区仙林大道163号, 南京大学仙林校区, 软件新技术国家重点实验室, 210023)
+                </p>
+            <div id="footer">
+                <div id="footer-text">
+                    Page generated 2020-09-1 14:54:40 CST, by <a href="http://lamda.nju.edu.cn/zhengqc/">Qin-Cheng Zheng</a>.
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
